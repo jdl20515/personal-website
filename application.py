f38c73474a278f10a9104df2e5da4b710720102a
jdl20515 <jdavidlomelin@gmail.com>
2021-11-24 21:15:46 -0500

    index and about me app routes

--- application.py
+++ application.py
@@ -25,3 +25,14 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutme.html")
+
+
